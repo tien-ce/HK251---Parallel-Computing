f6cef470f4f04d92c0b5390b82d09dcf7fe3fecb
tien-ce <tien.nguyen11102004@hcmut.edu.vn>
2025-12-03 05:12:15 +0100

    read write matrix

---
+++ LAB3/Components/Read_Write_Matrix/Include/read_write_matrix.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#ifndef READ_MATRIX_H
+#define READ_MATRIX_H
+int read_matrix(const char *filename, const int *rows, const int * cols, float ***matrix);
+int write_matrix(const char *filename, int rows, int cols, float ***matrix);
+int read_matrix1D(const char *filename, const int *rows, const int * cols, float *matrix);
+int write_matrix1D(const char *filename, int rows, int cols, float *matrix);
+#endif
