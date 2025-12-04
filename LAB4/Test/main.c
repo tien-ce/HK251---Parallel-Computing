f46d69a05404035b62aef58f2b2ecf73943b529b
tien-ce <tien.nguyen11102004@hcmut.edu.vn>
2025-12-04 10:22:29 +0100

    test cluster

---
+++ LAB4/Test/main.c
@@ -0,0 +1,11 @@
+#include <mpi.h>
+#include <stdio.h>
+int main (int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+    int world_rank, world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    printf("Hello from rank %d out of %d processors\n", world_rank, world_size);
+    MPI_Finalize();
+    return 0;
+}
